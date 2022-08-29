33e717b6b8d4a0dbe07d7ab78ac731e2ba1d7922
Светлана М <karenina2909@mail.ru>
2022-08-29 19:08:42 +0300

    сохранение зад2

---
+++ зад2/Program.cs
@@ -0,0 +1,17 @@
+﻿// по заданному номеру четверти покажет диапазон координат точек
+ void Qua(int quadrant)
+{
+    string output = "некорректный ввод данных";
+    if (quadrant==1)
+    output = $"для четверти {quadrant} x>0 y>0";
+    if (quadrant==2)
+    output = $"для четверти {quadrant} x<0 y>0";
+    if (quadrant==3)
+    output = $"для четверти {quadrant} x<0 y<0";
+    if (quadrant==4)
+    output = $"для четверти {quadrant} x>0 y<0";
+    Console.WriteLine(output);
+}
+Console.WriteLine("Введите номер четверти: ");
+int qua = Convert.ToInt32(Console.ReadLine());
+Qua(qua);
